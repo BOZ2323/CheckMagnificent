2a32cc87a47aef8f2d24eac3613c3b3401600ada
Lilli Baaske <baaske@architrave.de>
2020-09-06 14:39:57 +0200

    Show results of checks in terminal

--- checkMagnificent.js
+++ checkMagnificent.js
@@ -1,13 +1,28 @@
 const axios = require("axios");
-const {argv} = require('yargs')
+const { argv } = require("yargs");
+
+let url = "http://localhost:12345";
+let countAll = 0;
+let countErrors = 0;
+let countSuccess = 0;
 
 async function makeRequest() {
+  axios
+    .get(url)
+    .then((res) => {
+      countSuccess++;
+      countAll++;
+      console.log(`${res.data} Successful check!`);
+      console.log(`Total: ${countAll}  Failed: ${countErrors}  Successful: ${countSuccess}`);
+    })
+    .catch(function (error) {
+      if (error) {
+        countErrors++;
+        countAll++;
+        console.log(`Request failed with status code ${error.response.status}`);
+        console.log(`Total: ${countAll}  Failed: ${countErrors}  Successful: ${countSuccess}`);
+      }
+    });
+}
 
-
-    axios('http://localhost:12345')	    
-    .then(res => console.log(res.status))	    
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err));	    
-
-}	
 setInterval(makeRequest, argv.seconds * 1000);
