a9b197782db5e60be4791317c3bf042ba1fe58c9
Lilli Baaske <baaske@architrave.de>
2020-09-06 14:44:37 +0200

    Remove total number of checks in output to meet requirements

--- checkMagnificent.js
+++ checkMagnificent.js
@@ -2,7 +2,6 @@ const axios = require("axios");
 const { argv } = require("yargs");
 
 let url = "http://localhost:12345";
-let countAll = 0;
 let countErrors = 0;
 let countSuccess = 0;
 
@@ -11,16 +10,14 @@ async function makeRequest() {
     .get(url)
     .then((res) => {
       countSuccess++;
-      countAll++;
       console.log(`${res.data} Successful check!`);
-      console.log(`Total: ${countAll}  Failed: ${countErrors}  Successful: ${countSuccess}`);
+      console.log(`Failed: ${countErrors}  Successful: ${countSuccess}`);
     })
     .catch(function (error) {
       if (error) {
         countErrors++;
-        countAll++;
         console.log(`Request failed with status code ${error.response.status}`);
-        console.log(`Total: ${countAll}  Failed: ${countErrors}  Successful: ${countSuccess}`);
+        console.log(`Failed: ${countErrors}  Successful: ${countSuccess}`);
       }
     });
 }
